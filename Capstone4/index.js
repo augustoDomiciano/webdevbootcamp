affaeb2964c8c22e14cfa53c70ee1694286b024d
Augusto <augustoo.rd@gmail.com>
2024-09-12 23:11:37 -0300

    capstone4

---
+++ Capstone4/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    const result = await axios.get("https://v2.jokeapi.dev/joke/Any?type=single");
+    res.render("index.ejs", {
+      joke: result.data.joke,
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
